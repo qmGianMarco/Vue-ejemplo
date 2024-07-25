010a38201699efc67aad78c9811eddc936ba2ced
qmGianMarco <gian30marco12@gmail.com>
2024-07-25 16:11:26 -0500

    proyecto para aprender vue.js

---
+++ guitarla-vue/src/components/Guitarra.vue
@@ -0,0 +1,34 @@
+<script setup>
+    import { ref } from "vue";
+
+    const numero = ref(0)
+
+    const props = defineProps({ /* Permite conocer al componente la propiedad que le envian */
+        guitarra: {
+            type: Object,
+            required: true
+        }
+    })
+
+    defineEmits(['agregar-carrito'])
+
+</script>
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img class="img-fluid" :src="'/img/' + guitarra.imagen + '.jpg'" alt="imagen guitarra">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarra.nombre}}</h3>
+            <p>{{ numero }}</p>
+            <p>{{guitarra.descripcion}}</p>
+            <p class="fw-black text-primary fs-3">{{guitarra.precio}}</p>
+            <button 
+                type="button"
+                class="btn btn-dark w-100 "
+                @click="$emit('agregar-carrito', guitarra)"
+            >Agregar al Carrito</button>
+        </div>
+    </div><!-- FIN GUITARRA -->
+</template>
