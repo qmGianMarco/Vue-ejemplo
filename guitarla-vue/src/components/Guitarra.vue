e6e074dbaad722515d03ead5b2b5a714c2d9120b
qmGianMarco <gian30marco12@gmail.com>
2024-07-30 16:07:34 -0500

    Creacion de funcionalidades con Emits y Props

--- guitarla-vue/src/components/Guitarra.vue
+++ guitarla-vue/src/components/Guitarra.vue
@@ -1,7 +1,4 @@
 <script setup>
-    import { ref } from "vue";
-
-    const numero = ref(0)
 
     const props = defineProps({ /* Permite conocer al componente la propiedad que le envian */
         guitarra: {
@@ -17,13 +14,12 @@
 <template>
     <div class="col-md-6 col-lg-4 my-4 row align-items-center">
         <div class="col-4">
-            <img class="img-fluid" :src="'/img/' + guitarra.imagen + '.jpg'" alt="imagen guitarra">
+            <img class="img-fluid" :src="'/img/' + guitarra.imagen + '.jpg'" :alt="'imagen guitarra' + guitarra.nombre">
         </div>
         <div class="col-8">
-            <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarra.nombre}}</h3>
-            <p>{{ numero }}</p>
-            <p>{{guitarra.descripcion}}</p>
-            <p class="fw-black text-primary fs-3">{{guitarra.precio}}</p>
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+            <p>{{ guitarra.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">$ {{ guitarra.precio }}</p>
             <button 
                 type="button"
                 class="btn btn-dark w-100 "
